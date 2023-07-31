151e831e97469290a73a91bf1928b990cdfe169b
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-31 16:27:18 +0300

    post posts

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {randomBytes} = require('crypto')
 
 const app = express()
 
@@ -9,6 +10,14 @@ app.get('/posts', (req,res)=>{
 })
 
 app.post('/posts', (req,res)=>{
+    const id = randomBytes(4).toString('hex')
+    const { title} = req.body
+
+    posts[id] = {
+        id,title
+    }
+
+    res.status(201).send(posts[id])
     
 })
 app.listen(4000, ()=>{
